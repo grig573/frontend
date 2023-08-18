50765047d9accb083940d1d0aa900568cc8a23c9
grig573 <grigoryan200302@gmail.com>
2023-08-18 19:16:50 +0400

    creating routhers and context

---
+++ src/context/AuthContext.jsx
@@ -0,0 +1,37 @@
+import { createContext, useContext, useState } from "react";
+import axios from "axios";
+
+const AuthContent = createContext({
+    user: null,
+    setUser: () => {},
+    csrfToken: () => {},
+});
+
+export const AuthProvider = ({chilldren}) => {
+    const [user, _setUser] = useState(JSON.parse(localStorage.getItem('user')) || null);
+    
+    const setUser = (user) => {
+        if (user) {
+            localStorage.setItem('user', JSON.stringify(user));
+        }else{
+            localStorage.removeItem('user');
+        }
+        _setUser(user);
+    };
+
+    const csrfToken = async () => {
+        await axios.get('http://localhost:8000/sanctum/csrf-cookie');
+        return true;
+    };
+
+    return (
+
+        <AuthContent.Provider value = {{ user, setUser, csrfToken }}>
+        {chilldren}
+        </AuthContent.Provider>
+    );
+};
+
+export const useAuth = () => {
+    return useContext(AuthContent);
+};
---
+++ src/router.js
@@ -0,0 +1,41 @@
+import { createBrowserRouter } from "react-router-dom";
+import Login from './pages/Login';
+import Register from './pages/Register';
+import About from './pages/About';
+import Profile from './pages/Profile';
+import ProtectedLayout from './components/ProtectedLayout';
+import GuestLayout from './components/GuestLayout';
+
+const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <GuestLayout />,
+      children: [
+          {
+            path: '/',
+            element: <Login />,
+          },
+          {
+            path:'/register',
+            element:<Register />,
+          }
+      ]
+    },
+    {
+        path: '/',
+        element: <ProtectedLayout />,
+        children: [
+            {
+                path: '/about',
+                element: <About />,
+            },
+            {
+                path: '/profile',
+                element: <Profile />
+            }
+        ]
+    }
+
+ ])
+
+ export default router;
